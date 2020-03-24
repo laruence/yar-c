c97b14c02cf23d8cf4d1f271b1c40cf45a6266e2
Xinchen Hui <laruence@gmail.com>
2020-03-24 19:36:47 +0800

    Enable php client test

---
+++ example/client.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+$hostname = $argv[1];
+$client = new Yar_Client("tcp://" . $hostname);
+$client->SetOpt(YAR_OPT_CONNECT_TIMEOUT, 100);
+
+$result = $client->default(array(
+    1, 2,
+    'a' => 4
+));
+
+if (is_array($result)) {
+	var_dump($result);
+	exit(0);
+}
+exit(1);
